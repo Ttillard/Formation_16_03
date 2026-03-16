951832d2e5039af959557a8f436b5f647866fe5e
thibault <thibault.tillard@insee.fr>
2026-03-16 10:51:02 +0000

    modi scripts 1 & 2

--- scripts/script1.R
+++
@@ -1,3 +0,0 @@
-levels_a17 = c('AZ','C1','C2','C3','C4','C5','DE','FZ','GZ','HZ','IZ','JZ','KZ','LZ','MN','OQ','RU')
-
-secteurs_a17_melodi = c('RTU','OTQ','M_N','L','K','J','I','H','G','F','C_OTH','C29_30','C26T28','C19','C10T12','B_D_E','A')
